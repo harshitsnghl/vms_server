28abeddba54b8fe62602a01457fc48d286918142
Harshit Singhal <harshitsnghl@gmail.com>
2021-08-28 12:10:31 +0530

    Commit

---
+++ models/dbconnection.js
@@ -0,0 +1,15 @@
+// ****** Set up default mongoose connection START ****** //
+const mongoose = require('mongoose');
+var mongoDB = process.env.mongoDBURL;
+mongoose.connect(mongoDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+});
+
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+mongoose.connection.once('open', () => {
+    console.log('conneted to MONGODB- ElishERP database');
+});
+module.exports = mongoose;
+// ****** Set up default mongoose connection END ****** //
